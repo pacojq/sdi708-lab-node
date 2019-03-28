1b5e73181af74e7e5794db6007b46fde5a563744
PJ <pacojuanquiros@gmail.com>
2019-03-28 01:53:56 +0100

    SDI-NodeJS-8.5,67- Registro de usuario, autenticación y uso de sesión

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 var express = require('express');
 var app = express();
 
+var expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
+var crypto = require('crypto');
+
 var fileUpload = require('express-fileupload');
 app.use(fileUpload());
 
@@ -24,7 +33,8 @@ app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00-o33vp.mongodb.net:27017,'
     +   'tiendamusica-shard-00-01-o33vp.mongodb.net:27017,'
     +   'tiendamusica-shard-00-02-o33vp.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
-
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 
 
 //Rutas/controladores por lógica
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -22,9 +22,12 @@ module.exports = function(app, swig, gestorBD) {
     });
 
     app.get('/canciones/agregar', function (req, res) {
-        var respuesta = swig.renderFile('views/bagregar.html', {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
 
-        });
+        var respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
     });
 
@@ -45,10 +48,16 @@ module.exports = function(app, swig, gestorBD) {
 
     app.post("/cancion", function(req, res) {
 
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
+
         var cancion = {
             nombre : req.body.nombre,
             genero : req.body.genero,
-            precio : req.body.precio
+            precio : req.body.precio,
+            autor: req.session.usuario
         };
 
         // Conectarse
